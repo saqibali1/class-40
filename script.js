84d0a2ae57ab076efd6234c31bfba9c40ca06ac1
VB Student <vbstudent@uit.edu>
2018-06-26 17:54:58 +0500

    class-40

---
+++ script.js
@@ -0,0 +1,179 @@
+ // function counting(){
+ // 	document.querySelector()
+ // (let counting=10;counting<=100;counting++)
+
+	// console.log(counting.para);
+ // }
+
+  // int clicks = 0;
+  //   function click() {
+  //       clicks += 1;
+  //       document.getElementById("clicks").innerHTML = clicks;	
+
+	
+
+
+
+
+// for(let i=90 ; i<=100; i++){
+// console.log(i);
+// console.log("Hello");
+// }
+
+
+
+// if(1 !== 1){
+// 	console.log("hi");
+// 	console.log("hello");
+// }
+
+// if(1 !== 1)
+// 	console.log("hi")
+// 	console.log("hi2")
+
+
+
+// function select(){
+// 	for(let i=1; i<=100;i++)
+// 		para.innerHTML
+
+// 		document.write(i);
+// }
+
+
+// function print(){
+// 	for (let i=1; i<=100; i++){
+// 		let bodyEl =document.querySelector("body")
+// 		bodyEl.innerHTML= bodyEl.innerHTML + "<p>" + i + "</p>";
+
+// 	}
+// }
+
+
+// function startValue=$2
+// stopValue=$1
+
+
+// if [ $# -ne 2  ]
+// then
+//    echo "Error - You must enter two numbers exactly - using default start value of 1"
+// #exit 0
+// fi
+
+// echo ${startvalue:=1}
+
+// while (test "$startvalue" -le "$stopvalue" ||  "$startvalue" -ge "$stopvalue")
+// do
+//        startvalue=$((startvalue+1))
+//         console.log( $startvalue)
+
+
+
+
+
+function count(){
+	let div = document.querySelector("div");
+
+	div.innerHTML = "";
+
+    let body = document.querySelector("body");
+
+    let stop = document.getElementById("stopValue").value; 
+
+    start = document.getElementById("startValue").value;
+    
+    for(let x = start   ;x<=stop ;x++){
+
+    	 let bodyEl=document.querySelector("body");
+
+    	div.innerHTML += "<p>" + x + "</p>"
+
+     
+
+    }
+}
+
+
+
+    // document.write(x + " , ");
+         // // x=  x +" , "+ x + "";
+
+// let sentence ="1";
+// for(let number=2;number<=1000;number++){
+// 	sentence=  sentence +" , "+ number + "";
+// }
+
+
+// for(let number=11;number<=15;number++){
+// 	number=  number+" ~ "+ number ;
+// 	console.log(number );
+//  }
+
+
+// for(let number=10;number<=20;number++){
+// 	console.log("this is number is "  + number);
+//  }
+
+// for(let number=20;number<=30;number++){
+// 	console.log("this is number is "  + number);
+//  }
+
+// for(let number=30;number<=40;number++){
+// 	console.log("this is number is "  + number);
+//  }
+
+// for(let number=40;number<=50;number++){
+// 	console.log("this is number is "  + number);
+//  }
+
+// for(let number=50;number<=60;number++){
+// 	console.log("this is number is "  + number);
+//  }
+
+
+
+// 11 ~ 12 ~ 13 ~ 14 ~ 15
+// 21 ~ 22 ~ 23 ~ 24 ~ 25
+// 31 ~ 32 ~ 33 ~ 34 ~ 35
+// 41 ~ 42 ~ 43 ~ 44 ~ 45
+// 51 ~ 52 ~ 53 ~ 54 ~ 55
+// 61 ~ 62 ~ 63 ~ 64 ~ 65
+// 71 ~ 72 ~ 73 ~ 74 ~ 75
+// 81 ~ 82 ~ 83 ~ 84 ~ 85
+
+
+// 	let sentence="2";
+// 	for(let number=4;number<=99;number++){
+// 	if ((number % 2) == 0)
+// 		sentence=  sentence+" , "+number ;
+	 	
+// }
+
+//   console.log(sentence);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
